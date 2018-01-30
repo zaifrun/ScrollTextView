9ff14d8934e90c680607206d266ec0641122e493
zaifrun <martinknud@gmail.com>
2018-01-30 18:48:03 +0100

    Initial commit

---
+++ app/src/main/java/org/example/scrolltextview/MainActivity.java
@@ -0,0 +1,16 @@
+package org.example.scrolltextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView view = findViewById(R.id.textview);
+        view.setSelected(true);
+    }
+}
